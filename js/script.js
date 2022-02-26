a7412551833c8ddec71ce0408c5a94ebdc87c0ab
Noelia-Elizabeth <elizabeth.nmacleod@gmail.com>
2022-02-26 11:54:45 -0300

    Completo función strtTime para mostra fecha.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 
+console.log(new Date().getDate());
+
 function startTime() {
     var today = new Date();
     var hr = today.getHours();
@@ -8,6 +10,16 @@ function startTime() {
     min = checkTime(min);
     sec = checkTime(sec);
     document.getElementById("clock").innerHTML = hr + " : " + min + " : " + sec;
+
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    var curWeekDay = days[today.getDay()];
+    var curDay = today.getDate();
+    var curMonth = months[today.getMonth()];
+    var curYear = today.getFullYear();
+    var date = curWeekDay+", "+curDay+" "+curMonth+" "+curYear;
+    document.getElementById("date").innerHTML = date;
+
     var time = setTimeout(function(){ startTime() }, 1000);
 }
 
